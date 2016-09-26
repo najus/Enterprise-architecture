745452d94438d7976a075aa869bcaedb735199a4
Sujan <najussujan@gmail.com>
2016-09-25 22:39:04 -0400

    week 12

---
+++ week12/week12-ejb/src/java/ejb/NewsEntityFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * 
+ * @author najus
+ */
+
+@Stateless
+public class NewsEntityFacade extends AbstractFacade<NewsEntity> {
+    @PersistenceContext(unitName = "week12-ejbPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public NewsEntityFacade() {
+        super(NewsEntity.class);
+    }
+    
+}
